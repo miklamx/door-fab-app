2f9e9ca590a6f93e560c5b8a01fd8e5f3792c4d5
miklamx <mike.lamoreaux@gmail.com>
2026-04-12 11:17:20 -0600

    Remove comments related to GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,7 @@
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  // Required for GitHub Pages project sites like:
-  // https://miklamx.github.io/door-fab-app/
   base: '/door-fab-app/',
 })
